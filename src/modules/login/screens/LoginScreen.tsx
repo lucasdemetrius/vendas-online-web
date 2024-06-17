bdfc1652ad6eb565af07d489ce113fde9c070fe6
Lucas Jesus <lucas.jesus@flowti.com.br>
2024-06-17 15:00:06 -0300

    Feature: Create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,13 @@
+import { BackgroundImage } from "../styles/loginScreen.styles";
+
+const LoginScreen = () => {
+
+    return(
+        <div>
+            <BackgroundImage src="./background.png" />
+        </div>
+    )
+
+};
+
+export default LoginScreen;
